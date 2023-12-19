97e28d9c9685e049a2ddfd82b1a9faf13b10c759
Yassine B <yassinemens53@gmail.com>
2023-12-19 14:25:36 +0100

    lijnen gemaakt

--- 01 Start/02 lijnen/app.js
+++ 01 Start/02 lijnen/app.js
@@ -2,21 +2,30 @@ class App
 {
     runApplication()
     {
-        console.log("hello world");
-        let canvas = document.getElementById("canvasId");
-
-        let g = canvas.getContext("2d");
-
-        g.fillStyle = "blue";
-        g.fillRect(0,0,canvas.width,canvas.height);
-
-        g.fillStyle = "yellow";
-        g.fillRect(0,0,10,10);  
-
-        console.log(canvas);
+        console.log("hello world!")
 
 
-    }
+            let canvas = document.getElementById("canvasId"); 
+            console.log(canvas);
+
+
+    let g = canvas.getContext("2d");
+
+    g.beginPath();
+    g.moveTo(20,20)
+    g.lineTo(300,300)
+    g.stroke();
+
+    g.beginPath
+    g.fillStyle  = "red";
+    g.moveTo(20,20);
+    g.lineTo(300,300);
+    g.lineTo(580,20);
+    g.closePath();
+    g.stroke();
+    g.fill();
+
+}
 }
 
 let app = new App();
