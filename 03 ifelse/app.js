8043d243637c0459c368fb8ade7e9519bf09bd35
Yassine B <yassinemens53@gmail.com>
2023-12-21 14:11:47 +0100

    3,1 gemaakt

---
+++ 03 ifelse/app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+        document.getElementById("newstitle");
+        document.getElementById("gamenews headline");
+        document.getElementById("gamenews");
+    }
+}
+
+let app = new App();
+app.runApplication();
