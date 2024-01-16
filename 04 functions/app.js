8ce9c8efe8df13cf4abf8e828b88bd4217a183a2
Yassine B <yassinemens53@gmail.com>
2024-01-16 14:29:16 +0100

    gemaakt

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,3 +1,14 @@
+function ArgumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log("Shout out spree")
+}
+ArgumentsAreHandy();
+ArgumentsAreHandy();
+ArgumentsAreHandy();
+
+
+
 function GlobalFunction()
 {
     console.log("ik ben global")
