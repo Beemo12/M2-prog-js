5b9257605635b6bc02182f369bd994b39e9c249c
Yassine B <yassinemens53@gmail.com>
2024-01-25 09:02:36 +0100

    afgemaakt

--- 03 return/app.js
+++ 03 return/app.js
@@ -17,7 +17,7 @@ function ax2bcWiskunde(x, a, b, c) {
 let y1 = ax2bcWiskunde(9, 3, 4, 89);
 console.log(y1);
 
-let y2 = ax2bcWiskunde(3, 6, 5, 45);
+let y2 = ax2bcWiskunde(3, 6, 5, 45); 
 console.log(y2);
 
 let y3 = ax2bcWiskunde(7, 1, 2, 13);
@@ -27,7 +27,7 @@ console.log(y3);
 function wikunde2(u, h, l) {
     let logY = Math.log(l) + h + Math.pow(u, 2);
     return logY;
-}
+} 
 
 let logY1 = wikunde2(9, 8, 15);
 console.log(logY1)
